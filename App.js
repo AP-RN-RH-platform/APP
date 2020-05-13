cf43ebab64dd53206e23136c52b422a4b29b2f02
OmarAbdallaa <omar.abdallaa@gmail.com>
2020-05-13 13:35:52 +0200

    add forgotscreen
    Signed-off-by: OmarAbdallaa <omar.abdallaa@gmail.com>

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
 import HomeScreen from "./src/screens/HomeScreen";
+import ForgotPasswordScreen from "./src/screens/ForgotPasswordScreen";
 
 
 const Stack = createStackNavigator();
@@ -23,7 +24,7 @@ const App: () => React$Node = () => {
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Register" component={RegisterScreen} />
-          <Stack.Screen name="Forgot" component={ForgotPasswordScreen} />
+          <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
 
 
         </Stack.Navigator>
