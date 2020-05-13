e74613feb01064d44115cb6cbebe265fef6d09f4
Redha AISSAOUI <aissaoui.redha@gmail.com>
2020-05-14 00:08:50 +0200

    login

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
 import HomeScreen from "./src/screens/HomeScreen";
 import ForgotPasswordScreen from "./src/screens/ForgotPasswordScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
 
 
 const Stack = createStackNavigator();
@@ -25,6 +26,8 @@ const App: () => React$Node = () => {
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Register" component={RegisterScreen} />
           <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+
+          <Stack.Screen name="Profile" component={ProfileScreen} />
 
 
         </Stack.Navigator>
